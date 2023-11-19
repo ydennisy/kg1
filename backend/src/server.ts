e7b28d55e79c25c830c9eb6e38b4b677cdd8e31f
ydennisy <dennis@airgrid.io>
2023-11-19 16:49:15 +0000

    chore: pass a explicit host to fastify

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,10 @@ import { app } from './app';
 
 const start = async () => {
   try {
-    await app.listen({ port: Number(process.env.PORT) || 8000 });
+    await app.listen({
+      port: Number(process.env.PORT) || 8000,
+      host: '0.0.0.0',
+    });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
