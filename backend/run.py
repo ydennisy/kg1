e4896a1de5c8deed2f2dae756566e06b16a45c7d
ydennisy <dennis@airgrid.io>
2024-04-20 11:51:22 +0100

    chore: remove refs from the code

--- backend/run.py
+++ backend/run.py
@@ -2,12 +2,6 @@ import os
 import sys
 from uvicorn import run
 
-# TODO: fix it!
-# https://github.com/alexdmoss/distroless-python/tree/main/tests/fastapi
-# https://github.com/GoogleContainerTools/distroless/blob/main/examples/python3/Dockerfile
-# https://alexos.dev/2022/07/08/creating-an-up-to-date-distroless-python-image/
-# https://groups.google.com/g/distroless-users/c/1WYBzcsggk8
-
 PORT = int(os.getenv("PORT", "8080"))
 HOST = os.getenv("HOST", "0.0.0.0")
 
