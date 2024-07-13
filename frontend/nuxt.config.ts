f1314dca965fd4f99d0c97b63a18f933dc7718d3
ydennisy <dennis@airgrid.io>
2024-07-13 13:53:57 +0100

    chore: bump nuxt and a few other deps

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   // NOTE: this would allow moving everything into src!
   // srcDir: "src/",
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@pinia/nuxt'],
+
   css: ['~/assets/css/main.css'],
+
   runtimeConfig: {
     public: {
       apiBase: '',
@@ -12,6 +15,7 @@ export default defineNuxtConfig({
       supabaseAnonKey: '',
     },
   },
+
   routeRules: {
     '/': { static: true },
     '/**': { ssr: false },
@@ -22,10 +26,12 @@ export default defineNuxtConfig({
       proxy: 'http://127.0.0.1:8000/openapi.json',
     },
   },
+
   alias: {
     // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
     pinia: '/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs',
   },
+
   supabase: {
     redirectOptions: {
       login: '/login',
@@ -34,4 +40,6 @@ export default defineNuxtConfig({
       cookieRedirect: true,
     },
   },
+
+  compatibilityDate: '2024-07-13',
 });
